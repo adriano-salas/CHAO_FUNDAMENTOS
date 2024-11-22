dbb680722a797dfa53c60453779789bd7aa0af07
UPN <UPN@ULCLAB406>
2024-11-22 11:27:45 -0500

    Agregar archivos de proyecto.

---
+++ ejer1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejer1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese # filas: ");
+            int f= int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese # columnas: ");
+            int c= int.Parse(Console.ReadLine());
+
+            int[,] matris = new int[f,c];
+            Random rnd = new Random();
+            for (int i = 0; i < matris.GetLength(0) ; i++)
+            {
+                for (int j = 0; j < matris.GetLength(1) ; j++)
+                {
+                    matris[i,j] = rnd.Next(30,51);
+                    Console.Write(matris[i,j]+"\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
